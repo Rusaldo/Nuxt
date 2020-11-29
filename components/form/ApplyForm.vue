d8d7f06eeaed30e16f86b77c5fdf31f64ecbcd61
yara <Ruslan.Yandiev@technopark.ru>
2020-11-29 22:05:06 +0300

    Form

--- components/form/ApplyForm.vue
+++ components/form/ApplyForm.vue
@@ -1,24 +1,30 @@
 <template>
-  <form class="apply-form" @submit.prevent="handleSubmit">
-    <h2 class="apply-form__title">Оставьте заявку</h2>
+  <form ref="form"
+        class="apply-form"
+        name="apply"
+        data-netlify=true
+        @submit.prevent="handleSubmit" >
+    <h2 class="apply-form__title">Оформить заявку на кредит</h2>
     <p class="apply-form__text">Наш менеджер поможет ответить на все интересующие вопросы, а также подобрать оптимальный способ получения кредита именно в вашем случае</p>
     <div class="apply-form__form-wrapper">
       <div class="apply-form__field">
         <BaseInput v-model="name"
                    type="text"
+                   name="name"
                    placeholder="Имя" />
       </div>
       <div class="apply-form__field">
         <BaseInput v-model="phone"
                    type="tel"
+                   name="phone"
                    placeholder="Номер телефона" />
       </div>
       <div class="apply-form__field">
-        <BaseButton type="primary" native-type="submit">Отправить заявку</BaseButton>
+        <BaseButton variant="accent" native-type="submit">Отправить заявку</BaseButton>
       </div>
     </div>
     <p class="apply-form__assent">
-      &#128221; Я даю согласие ООО «Русалдо» на обработку моих <a class="link" href="#">персональных данных</a>
+      &#128221; Я даю согласие ООО «Русалдо» на обработку моих <br> <a class="apply-form__link" href="#">персональных данных</a>
     </p>
   </form>
 </template>
@@ -26,13 +32,16 @@
 <script>
 import BaseInput from '~/components/base/BaseInput.vue'
 import BaseButton from '~/components/base/BaseButton.vue'
+import ApplySuccess from '~/components/ApplySuccess.vue'
+import ModalBus from '~/plugins/modalBus'
 
 export default {
   name: 'ApplyForm',
 
   components: {
     BaseInput,
-    BaseButton
+    BaseButton,
+    ApplySuccess
   },
 
   data() {
@@ -44,8 +53,34 @@ export default {
 
   methods: {
     handleSubmit() {
-      this.clearForm()
-      this.$emit('close')
+      if (!this.validate()) return
+      const form = this.$refs.form
+      console.log(form)
+      const formData = new FormData(form)
+      console.log(formData)
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(formData).toString()
+      })
+        .then(() => {
+          console.log('Form successfully submitted')
+          this.clearForm()
+          this.$emit('close')
+          this.onSuccess()
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    },
+    validate() {
+      return this.name.length && this.phone.length
+    },
+    onSuccess() {
+      ModalBus.$emit('open', {
+        component: ApplySuccess,
+        title: 'Ваша заявка принята'
+      })
     },
     clearForm() {
       this.name = ''
@@ -78,6 +113,14 @@ export default {
       &:not(:last-of-type) {
         margin-bottom: 24px;
       }
+    }
+
+    &__assent {
+      font-size: 14px;
+    }
+
+    &__link {
+      text-decoration: underline;
     }
   }
 
